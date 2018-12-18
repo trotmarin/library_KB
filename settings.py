bfa5b17529610a1790b9f8cd805f60ebe1745977
trotmarin <trotmarin@gmail.com>
2018-12-18 14:18:30 +0900

    4th

--- settings.py
+++ settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 't(bl0gujfx$ggal!rmuo&_uom9yia9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['protected-crag-35538.herokuapp.com','127.0.0.1']
 
 
 # Application definition
